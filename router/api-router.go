e6aa23a077a18a0f6f079466644d3ffb9f1f0d4c
qiang-wei <qiang_wei@apple.com>
2026-02-24 18:01:44 +0900

    feat: Implement log csv export.

--- router/api-router.go
+++ router/api-router.go
@@ -313,6 +313,7 @@ func SetApiRouter(router *gin.Engine) {
 		logRoute.GET("/search", middleware.AdminAuth(), controller.SearchAllLogs)
 		logRoute.GET("/self", middleware.UserAuth(), controller.GetUserLogs)
 		logRoute.GET("/self/search", middleware.UserAuth(), middleware.SearchRateLimit(), controller.SearchUserLogs)
+		logRoute.GET("/export/csv", middleware.AdminAuth(), controller.ExportAllLogs)
 
 		systemTaskRoute := apiRouter.Group("/system-task")
 		systemTaskRoute.Use(middleware.RootAuth())
